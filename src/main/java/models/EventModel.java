9be176a51b98b58322fcdb2bdece46a90883f3fe
Nafeesa Yousaf <nafeesayousaf2129@gmail.com>
2024-12-25 23:49:48 +0500

    create event model

--- src/main/java/models/EventModel.java
+++ src/main/java/models/EventModel.java
@@ -1,9 +1,71 @@
 package models;
 
-/**
- *
- * @author PMYLS
- */
 public class EventModel {
+    private String eventId;
+    private String name;
+    private String date; 
+    private String location;
+    private String description;
+    private String eventStatus; 
 
+    public EventModel() {
+    }
+
+    public EventModel(String eventId, String name, String date, String location, String description, String eventStatus) {
+        this.eventId = eventId;
+        this.name = name;
+        this.date = date;
+        this.location = location;
+        this.description = description;
+        this.eventStatus = eventStatus;
+    }
+
+    // Getters and Setters
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEventStatus() {
+        return eventStatus;
+    }
+
+    public void setEventStatus(String eventStatus) {
+        this.eventStatus = eventStatus;
+    }
 }
